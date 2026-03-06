615647ed7756f38757e875fdfd0862ecede524f4
P-Tanjim <moksedurtanjim27@gmail.com>
2026-03-07 00:56:16 +0600

    add navbar and make it responsive

--- script/login.js
+++ script/login.js
@@ -3,15 +3,13 @@ const signIn = document.querySelector("#sign-in");
 signIn.addEventListener("click", ()=> {
     const name = document.getElementById("name").value;
     const pass = document.getElementById("pass").value;
-    if (name === "admin" && pass === "admin123") {
+    if (name.trim() === "admin" && pass.trim() === "admin123") {
         window.location.assign('./dashboard.html');
-        console.log("work")
+        alert("You'r logged in");
         return
     }
     else if(name.trim() === "" || pass.trim() === ""){
         return
     }
-    
-    
     alert("your username or password is incorrect")
 });
