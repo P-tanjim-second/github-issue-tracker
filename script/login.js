865849a27c28a6895dc996cf8c092866b1f12d19
P-Tanjim <moksedurtanjim27@gmail.com>
2026-03-12 22:56:41 +0600

    fix the login problem

--- script/login.js
+++ script/login.js
@@ -1,10 +1,14 @@
 const signIn = document.querySelector("#sign-in");
 
-signIn.addEventListener("click", ()=> {
+signIn.addEventListener("click", (e)=> {
+    // e.preventDefault() na deo ay page auto reload hocchilo condition check er agei. ajonno kaj kortesilo na 
+    e.preventDefault();
     const name = document.getElementById("name").value;
     const pass = document.getElementById("pass").value;
     if (name.trim() === "admin" && pass.trim() === "admin123") {
-        window.location.assign('./dashboard.html');
+        // assign korar somoy "./" deoa jabe na
+        window.location.assign('dashboard.html');
+        // alert deo ay page wait kortesilo and ai somoy condition check hoye jacchilo
         alert("You'r logged in");
         return
     }
